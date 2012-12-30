ab5b0a3712aa9c8519e566765125188d0084f924
Andrew Sutherland <dr3wsuth3rland@gmail.com>
2012-12-29 18:32:17 -0600

    auto_hotplug: section mismatches

--- arch/arm/kernel/auto_hotplug.c
+++ arch/arm/kernel/auto_hotplug.c
@@ -202,7 +202,7 @@ static void hotplug_decision_work_fn(struct work_struct *work)
 
 }
 
-static void hotplug_online_all_work_fn(struct work_struct *work)
+static void __cpuinit hotplug_online_all_work_fn(struct work_struct *work)
 {
 	int cpu;
 	for_each_possible_cpu(cpu) {
@@ -229,7 +229,7 @@ static void hotplug_offline_all_work_fn(struct work_struct *work)
 	}
 }
 
-static void hotplug_online_single_work_fn(struct work_struct *work)
+static void __cpuinit hotplug_online_single_work_fn(struct work_struct *work)
 {
 	int cpu;
 
